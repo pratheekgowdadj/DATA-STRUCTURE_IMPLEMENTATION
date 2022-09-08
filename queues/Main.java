6876853c2b7b96f9f7fc82d3f73cc39eb5322591
[Pratheek Gowda] <pratheekgowdadj@gmail.com>
2022-09-08 18:52:20 +0530

    08/09/2022

---
+++ queues/Main.java
@@ -0,0 +1,12 @@
+package queues;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.enQueue(4);
+        q.enQueue(4);
+        q.enQueue(4);
+        q.enQueue(4);
+        q.enQueue(4);
+    }
+}
--- queues/Queue.java
+++ queues/Queue.java
@@ -1,4 +1,58 @@
 package queues;
 
 public class Queue {
+    int size = 5;
+    int front, rear;
+    int[] items = new int[size];
+
+    public Queue() {
+        front = -1;
+        rear = -1;
+    }
+
+    boolean isFull(){
+        if(front == 0 && rear == size -1) return true;
+        return false;
+    }
+
+    boolean isEmpty(){
+        if(front == -1) return true;
+        else return false;
+    }
+
+    void enQueue(int element){
+        if(isFull()) System.out.print("Queue is full");
+        else{
+            if(front == -1) front = 0;
+            rear = (rear + 1) % size;
+            items[rear] = element;
+        }
+    }
+    int deQueue(){
+        int element;
+        if(isEmpty()) {
+            System.out.print("Queue is empty");
+            return -1;
+        }
+        else {
+            element = items[front];
+            if(front == rear){
+                front = -1;
+                rear = -1;
+            }
+            else front = (front + 1 ) % size;
+            return element;
+        }
+    }
+
+    void display(){
+        int i;
+        if(isEmpty()) System.out.print("Empty queue");
+        else{
+            for( i = front; i != rear; i = (i + 1 ) % size)
+                System.out.print(items[i] + " ");
+            System.out.println(items[i]);
+        }
+    }
+
 }
