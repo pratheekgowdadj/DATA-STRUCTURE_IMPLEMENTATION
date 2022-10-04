cd558c478666d43c89a5c071ad0062d0f1c01830
[Pratheek Gowda] <pratheekgowdadj@gmail.com>
2022-10-04 17:24:10 +0530

    04/10/2022

--- randomn_practice/Bs.java
+++ randomn_practice/Bs.java
@@ -3,20 +3,23 @@ package randomn_practice;
 public class Bs    {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        System.out.println(BS(arr,10,0,arr.length - 1));
+        System.out.println(BS(arr,101,0,arr.length - 1));
     }
 
     private static int BS(int[] arr, int target, int s, int e) {
 
-        if(s > e) return -1;
-         int m = s + (e - s) / 2;
+        //if(s > e) return -1;
+        if (s <= e) {
 
-         if(arr[m] == target) return m;
-
-         else if(arr[m] > target) return BS(arr, target, s, m);
-
-         return BS(arr,target,m + 1,e);
+            int m = s + (e - s) / 2;
 
 
+            if (arr[m] == target) return m;
+
+            else if (arr[m] > target) return BS(arr, target, s, m);
+
+            else if(arr[m] < target) return BS(arr, target, m + 1, e);
+        }
+        return -1;
     }
 }
